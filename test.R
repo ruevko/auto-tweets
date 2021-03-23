0b7f9516f4c7f6f410afadc7e8af64320c70aaac
unknown <ruevko@gmail.com>
2021-03-23 13:11:38 -0500

    Update test.R functionality

--- test.R
+++ test.R
@@ -1,3 +1,5 @@
+stopifnot(file.exists("credentials.txt"))
+
 library(twitteR)
 
 credentials = readLines("credentials.txt")
@@ -9,6 +11,14 @@ setup_twitter_oauth(credentials[1],
                     credentials[3],
                     credentials[4])
 
-test = updateStatus("This is a\nTwitter #API test\n\u270C")
+newstatus = ifelse(file.exists("newstatus.txt"),
+                   paste(readLines("newstatus.txt"), collapse = "\n"),
+                   "This is my default\n#TwitterAPI test\n\u2728\u2728\u2728")
 
-deleteStatus(test)
+newstatus = updateStatus(newstatus)
+
+Sys.sleep(30)
+
+delstatus = deleteStatus(newstatus)
+
+if(delstatus) message("Success \u270C")
