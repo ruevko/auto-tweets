7caa1c62e7c52a968fe92ec6b029bc72fbc7848e
Rafael Castro <ruevko@gmail.com>
2021-03-29 12:59:33 -0500

    Use rtweet package

---
+++ tweet.R
@@ -0,0 +1,13 @@
+if (file.exists("resources/credentials.txt")) {
+   creds = readLines("resources/credentials.txt")
+} else creds = commandArgs(TRUE)
+
+if (length(creds) != 5) stop("Credentials in wrong amount or unavailable")
+
+library("rtweet", lib.loc = if (dir.exists("resources/library")) "resources/library")
+
+token = create_token(creds[1], creds[2], creds[3], creds[4], creds[5], set_renv = FALSE)
+
+if (file.exists("new_status.txt")) {
+   readLines("new_status.txt") %>% paste(collapse = "\n") %>% post_tweet(token = token)
+} else post_tweet(token = token)
