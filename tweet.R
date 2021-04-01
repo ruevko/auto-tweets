91a76a5656843a80787d8b7bae0a4cb0c2257ac6
unknown <ruevko@gmail.com>
2021-03-31 20:01:09 -0500

    First attempt to cache

--- tweet.R
+++ tweet.R
@@ -6,7 +6,8 @@ if (length(creds) != 5) stop("Credentials in wrong amount or unavailable")
 
 if (dir.exists("resources/library")) .libPaths("resources/library")
 
-library("rtweet")
+message("Using ", R.version.string)
+library("rtweet"); message("...and rtweet version ", packageVersion("rtweet"))
 
 token = create_token(creds[1], creds[2], creds[3], creds[4], creds[5], set_renv = FALSE)
 
